ddd367ff5f3bae82d5f7e76e78fc3b0bc5e2d798
oochan <oochanbg@gmail.com>
2020-02-17 16:12:14 +0630

    local components

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import HelloWorld from './components/HelloWorld'
 
 Vue.config.productionTip = false
 
+Vue.component('app-hello-world',HelloWorld);
 new Vue({
   render: h => h(App),
 }).$mount('#app')
